f13fd3173c9ad09d17ab06588ce03f504c7b32c3
Xavier Re4 <xrthefrench@gmail.com>
2019-02-21 16:12:28 +0100

    4

--- script_2.js
+++ script_2.js
@@ -1,7 +1,8 @@
 var n = prompt("De quel nombre veut tu calculer la factorielle ?");
+
 function factorial(n) {
   return (n != 1) ? n * factorial(n - 1) : 1;
 }
 
-alert( factorial(n) );
-console.log( factorial(n) );
+alert(factorial(n));
+console.log(factorial(n));
--- script_3.js
+++ script_3.js
@@ -1,8 +1,18 @@
-var var_num = prompt("salut petit coquinou combien d'etages veux-tu ?");
-function pyramid(i)
-{
-  i=="#"
-while var_num => i
-		 do
-    console.log ("#")
+var stageNumber = prompt("salut petit coquinou combien d'etages veux-tu ?")
+console.log("voici ta pyramide")
+
+function marioPyramid(stageNumber) {
+  let stars = "*"
+  while (stageNumber > 0) {
+    let emptiness = ""
+    let stageNumberLine = stageNumber - 1
+    while (stageNumberLine > 0) {
+      emptiness += " "
+      stageNumberLine -= 1
+    }
+    console.log(emptiness + stars)
+    stars += "*"
+    stageNumber -= 1
+  }
 }
+console.log(marioPyramid(stageNumber));
